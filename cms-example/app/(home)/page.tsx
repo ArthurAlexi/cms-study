c3a471dcfe4fec3efecc6ef2625415767f101f82
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-09-30 20:10:37 -0300

    feat: start to adapte the home page to CMS

--- cms-example/app/(home)/page.tsx
+++ cms-example/app/(home)/page.tsx
@@ -1,26 +1,40 @@
+import MainCharacter from "@/models/mainCharacter";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const mainTheme = await MainCharacter.findById('66fb269f920cf14e7dd9bbc5')
+  console.log(mainTheme)
+
+  
+  if (mainTheme?.page && mainTheme.page?.length < 0) {
+    return <h1>Não há paginas criadas</h1>
+  }
+
   return (
-     <main className="container mx-auto flex h-[70vh] items-center  justify-around flex-wrap gap-4">
-        <div className="max-w-md space-y-6">
-          <h1 className="text-6xl font-bold text-primary">
-            Fake enterprise
-          </h1>
-          <h2 className="text-xl font-semibold text-secondary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur illo, odio neque inventore reiciendis velit delectus ullam quia dignissimos 
-          </h2>
+    <main className="container mx-auto flex h-[70vh] items-center  justify-around flex-wrap gap-4">
+      <div className="max-w-md space-y-6">
+        <h1 className="text-6xl font-bold text-primary">
+          {
+            mainTheme && mainTheme.page![0].title
+          }
+        </h1>
+        <h2 className="text-xl font-semibold text-secondary">
+          {
+            mainTheme && mainTheme.page![0].subtitle
+          }
+        </h2>
 
-          <button className="px-8 py-2 bg-primary rounded-md text-white hover:bg-primary/80">
-            Lorem
-          </button>
-        </div>
+        <button className="px-8 py-2 bg-primary rounded-md text-white hover:bg-primary/80">
+          Lorem
+        </button>
+      </div>
 
-        <Image 
-          width={420}
-          height={200}
-          src='https://medrectech.com/image/enterprise.png' 
-          alt="enterpise image" />
-     </main> 
+      <Image
+        width={420}
+        height={200}
+        src={mainTheme.page![0].main_image_url}
+        alt="enterpise image" />
+    </main>
   );
 }
