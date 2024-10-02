9ebea4f220011a6cd753fffdd43e7c8f3de48daa
Arthur Alexi <arthuralexi@hotmail.com>
2024-10-01 22:15:27 -0300

    style: create a simple template page

--- cms-example/app/(home)/page.tsx
+++ cms-example/app/(home)/page.tsx
@@ -3,26 +3,16 @@ import Image from "next/image";
 
 export default async function Home() {
 
-  const mainTheme = await MainCharacter.findById('66fb269f920cf14e7dd9bbc5')
-  console.log(mainTheme)
-
   
-  if (mainTheme?.page && mainTheme.page?.length < 0) {
-    return <h1>Não há paginas criadas</h1>
-  }
 
   return (
     <main className="container mx-auto flex h-[70vh] items-center  justify-around flex-wrap gap-4">
       <div className="max-w-md space-y-6">
         <h1 className="text-6xl font-bold text-primary">
-          {
-            mainTheme && mainTheme.page![0].title
-          }
+          Fake Enterprise
         </h1>
         <h2 className="text-xl font-semibold text-secondary">
-          {
-            mainTheme && mainTheme.page![0].subtitle
-          }
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, repudiandae praesentium quibusdam minus temporibus inventore,
         </h2>
 
         <button className="px-8 py-2 bg-primary rounded-md text-white hover:bg-primary/80">
@@ -33,7 +23,7 @@ export default async function Home() {
       <Image
         width={420}
         height={200}
-        src={mainTheme.page![0].main_image_url}
+        src='https://medrectech.com/image/enterprise.png'
         alt="enterpise image" />
     </main>
   );
