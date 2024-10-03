d866ed8812fecadf7e23480f3bc40f7b8f9ec653
Arthur Alexi <arthuralexi@hotmail.com>
2024-10-02 21:00:23 -0300

    feat: change CSS Variables based in URL params

--- cms-example/app/[nameCharacter]/page.tsx
+++ cms-example/app/[nameCharacter]/page.tsx
@@ -1,20 +1,22 @@
 
+import General, { IGeneral } from "@/models/general"
 import Page, { IPage } from "@/models/page"
 import { HydratedDocument } from "mongoose"
 import Image from "next/image"
 
 type Params = {
     params: {
-      nameCharacter: string
+        nameCharacter: string
     }
-  }
+}
 
 export default async function CustomPage({ params }: Params) {
 
     const { nameCharacter } = params
     const pageData = await Page.findOne({ main_character_name: nameCharacter }) as HydratedDocument<IPage> || null
+    const themeData = await General.findOne({ main_character_name: nameCharacter }) as HydratedDocument<IGeneral> || null
 
-    if (!pageData) {
+    if (!pageData || !themeData) {
         return (
             <h1>
                 main_character_name not found
@@ -22,31 +24,44 @@ export default async function CustomPage({ params }: Params) {
         )
     }
 
+    const cssVariables = `
+    :root {
+      --background: ${themeData.background_color};
+      --foreground: ${themeData.foreground_color};
+      --primary: ${themeData.primary_color};
+      --secondary: ${themeData.secondary_color};
+      --textColor: ${themeData.text_color};
+    }
+  `
+
     return (
-        <main className="container mx-auto flex h-[70vh] items-center  justify-around flex-wrap gap-4">
+        <>
+            <style>{cssVariables}</style>
+            <main className="container mx-auto flex h-[70vh] items-center  justify-around flex-wrap gap-4">
 
-            <div className="max-w-md space-y-6">
-                <h1 className="text-6xl font-bold text-primary">
-                    {
-                       pageData.title
-                    }
-                </h1>
-                <h2 className="text-xl font-semibold text-secondary">
-                    {
-                        pageData.sub_title
-                    }
-                </h2>
+                <div className="max-w-md space-y-6">
+                    <h1 className="text-6xl font-bold text-primary">
+                        {
+                            pageData.title
+                        }
+                    </h1>
+                    <h2 className="text-xl font-semibold text-secondary">
+                        {
+                            pageData.sub_title
+                        }
+                    </h2>
 
-                <button className="px-8 py-2 bg-primary rounded-md text-white hover:bg-primary/80">
-                    Lorem
-                </button>
-            </div>
+                    <button className="px-8 py-2 bg-primary rounded-md text-white hover:bg-primary/80">
+                        Lorem
+                    </button>
+                </div>
 
-            <Image
-                width={420}
-                height={200}
-                src={pageData.main_image_url}
-                alt="enterpise image" />
-        </main>
+                <Image
+                    width={420}
+                    height={200}
+                    src={pageData.main_image_url}
+                    alt="enterpise image" />
+            </main>
+        </>
     )
 }
